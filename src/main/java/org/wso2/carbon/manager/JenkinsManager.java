8c3707a9fd470bb671c4c908f0e03786be7856df
arunasujith <sujithucsc@gmail.com>
2014-03-20 17:18:25 +0530

    add JenkinsManager.java file

---
+++ src/main/java/org/wso2/carbon/manager/JenkinsManager.java
@@ -0,0 +1,16 @@
+package org.wso2.carbon.manager;
+
+import org.wso2.carbon.data.JenkinsData;
+import org.wso2.carbon.processor.JenkinsProcessor;
+
+/**
+ * Created by aruna on 3/18/14.
+ */
+public class JenkinsManager {
+
+//    JenkinsProcessor jenkins = new JenkinsProcessor();
+//
+//    public void execute(String jobName){
+//        JenkinsData data = jenkins.getJenkinsData(jobName);
+//    }
+}
